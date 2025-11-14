cfca6169b0a728bd10f8ae0194ddac98ca439d75
yousseflab20-ui <yousseflab20@gmail.com>
2025-11-14 21:29:45 +0100

    modification your projets

--- app/mapApplication.tsx
+++ app/mapApplication.tsx
@@ -1,79 +1,100 @@
+import { casaLocations } from "@/data/casaLocations";
+import { calculateTime, calculation2 } from "@/utils/calculations";
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
+import { useFonts } from "expo-font";
 import * as Location from "expo-location";
-import React, { useEffect, useState } from "react";
-import { Alert, StyleSheet } from "react-native";
+import React, { ReactNode, useEffect, useState } from "react";
+import {
+  Alert,
+  StyleProp,
+  StyleSheet,
+  Text,
+  TextStyle,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import MapView, { Marker } from "react-native-maps";
-import { AVAILABLE_TAXIS, CASA_CENTER } from "../data/taxiData";
-
+import { AVAILABLE_TAXIS, CASA_CENTER, USER_POSITION } from "../data/taxiData";
 type MyLocation = {
   coords: {
     latitude: number;
     longitude: number;
   };
 };
+interface customText3 {
+  children?: ReactNode;
+  style?: StyleProp<TextStyle>;
+}
 export default function mapApplication() {
   const [userLocation, setUserLocation] = useState<MyLocation | null>(null);
-  const [loading, setLoading] = useState(true);
-
-  // Load custom font
-  const [fontsLoaded] = useFonts({
-    Font3: require("../font/MomoTrustDisplay-Regular.ttf"), // Path correct
-  });
-
   useEffect(() => {
     (async () => {
-      try {
-        let { status } = await Location.requestForegroundPermissionsAsync();
-        if (status !== "granted") {
-          Alert.alert("Permission refusée", "Activez la localisation pour continuer.");
-          setLoading(false);
-          return;
-        }
-
-        let currentLocation = await Location.getCurrentPositionAsync({
-          accuracy: Location.Accuracy.High,
-        });
-
-        setUserLocation(currentLocation);
-      } catch (error) {
-        console.log("Erreur:", error);
-        Alert.alert("Erreur", "Impossible d'obtenir la position actuelle.");
-      } finally {
-        setLoading(false);
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        Alert.alert("Permission refusée");
+        return;
       }
+      let currentLocation = await Location.getCurrentPositionAsync({});
+      setUserLocation(currentLocation);
     })();
   }, []);
-  return (
-    <MapView initialRegion={CASA_CENTER} style={{ flex: 1 }}>
-      <Marker coordinate={USER_POSITION} title="You are here" />
+  const [loading2] = useFonts({
+    Font3: require("../font/MomoTrustDisplay-Regular.ttf"),
+  });
+  const CustomText2 = ({ children, style }: customText3) => (
+    <View>
+      <Text style={{ fontFamily: "Font3" }}>{children}</Text>
+    </View>
+  );
+  const user = {
+    latitude: 33.5731,
+    longitude: -7.5898,
+  };
 
-      {AVAILABLE_TAXIS.map((taxi) => (
-        <Marker
-          key={taxi.id}
-          coordinate={{ latitude: taxi.latitude, longitude: taxi.longitude }}
-          title={`Taxi ${taxi.id}`}
-          pinColor="red"
-        />
-      ))}
-    </MapView>
+  const taxi = {
+    latitude: 33.5881,
+    longitude: -7.6843,
+  };
+  const distance = calculation2(user, taxi);
+  console.log(`${distance}`);
+
+  const duration = calculateTime(distance);
+  console.log(`${duration}$`);
+  return (
+    <View style={{ flex: 1 }}>
+      <MapView initialRegion={CASA_CENTER} style={{ flex: 1 }}>
+        <Marker coordinate={USER_POSITION} title="You are here" />
+
+        {AVAILABLE_TAXIS.map((taxi) => (
+          <Marker
+            key={taxi.id}
+            coordinate={{ latitude: taxi.latitude, longitude: taxi.longitude }}
+            title={`Taxi ${taxi.id}`}
+          >
+            <FontAwesome6 name="taxi" size={24} color="red" />
+          </Marker>
+        ))}
+        {casaLocations.map((check) => (
+          <Marker key={check.id} coordinate={check.point} title={check.name} />
+        ))}
+      </MapView>
+      <View style={{ left: 0, right: 0, alignItems: "center" }}>
+        <TouchableOpacity
+          style={{
+            backgroundColor: "#DC143C",
+            bottom: 80,
+            position: "absolute",
+            padding: 15,
+            borderRadius: 17,
+          }}
+        >
+          <CustomText2>🚖 Réserver un taxi</CustomText2>
+        </TouchableOpacity>
+      </View>
+    </View>
   );
 }
-
 const styles = StyleSheet.create({
+  container: { flex: 1 },
   map: { flex: 1 },
-  centered: { flex: 1, justifyContent: "center", alignItems: "center" },
-  buttonContainer: {
-    position: "absolute",
-    bottom: 50,
-    left: 0,
-    right: 0,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  button: {
-    backgroundColor: "#DC143C",
-    paddingVertical: 12,
-    paddingHorizontal: 25,
-    borderRadius: 15,
-    alignItems: "center",
-  },
 });
